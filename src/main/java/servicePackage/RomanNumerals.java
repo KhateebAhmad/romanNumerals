9b08a432e35a146ac14a610b1ab77a02cc037152
Farhan Ahmad <farhan.ahmad@travelportdigital.com>
2017-11-05 19:39:52 +0000

    Initial commit

---
+++ src/main/java/servicePackage/RomanNumerals.java
@@ -0,0 +1,77 @@
+package servicePackage;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RomanNumerals {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/convert")
+    public String convertToRoman(String inputNumber){
+
+        int input = Integer.parseInt(inputNumber);
+
+
+        if (input < 1 || input > 3999)
+            System.out.println("Invalid Roman Number Value");
+        String s = "";
+        while (input >= 1000) {
+            s += "M";
+            input -= 1000;        }
+        while (input >= 900) {
+            s += "CM";
+            input -= 900;
+        }
+        while (input >= 500) {
+            s += "D";
+            input -= 500;
+        }
+        while (input >= 400) {
+            s += "CD";
+            input -= 400;
+        }
+        while (input >= 100) {
+            s += "C";
+            input -= 100;
+        }
+        while (input >= 90) {
+            s += "XC";
+            input -= 90;
+        }
+        while (input >= 50) {
+            s += "L";
+            input -= 50;
+        }
+        while (input >= 40) {
+            s += "XL";
+            input -= 40;
+        }
+        while (input >= 10) {
+            s += "X";
+            input -= 10;
+        }
+        while (input >= 9) {
+            s += "IX";
+            input -= 9;
+        }
+        while (input >= 5) {
+            s += "V";
+            input -= 5;
+        }
+        while (input >= 4) {
+            s += "IV";
+            input -= 4;
+        }
+        while (input >= 1) {
+            s += "I";
+            input -= 1;
+        }
+
+        String output = "Input number "+inputNumber+" the corresponding number in Roman Numerals is: "+s;
+
+        return output;
+    }
+
+}
