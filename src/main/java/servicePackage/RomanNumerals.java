faf4da6df2ea06787fb44303ae2952321e819a10
KhateebAhmad <x16112989@student.ncirl.ie>
2017-11-06 20:33:16 +0000

    Refactoring

--- src/main/java/servicePackage/RomanNumerals.java
+++ src/main/java/servicePackage/RomanNumerals.java
@@ -12,66 +12,68 @@ public class RomanNumerals {
     public String convertToRoman(String inputNumber){
 
         int input = Integer.parseInt(inputNumber);
-
-
-        if (input < 1 || input > 3999)
-            System.out.println("Invalid Roman Number Value");
-        String s = "";
+        String output = "";
+        
+        if (input < 1 || input > 3999) {
+            output = "Invalid Roman Number Value";
+            return output;
+        }
+        
         while (input >= 1000) {
-            s += "M";
-            input -= 1000;        }
+            output = output + "M";
+            input = input - 1000;        
+        }
         while (input >= 900) {
-            s += "CM";
-            input -= 900;
+            output = output + "CM";
+            input = input - 900;
         }
         while (input >= 500) {
-            s += "D";
-            input -= 500;
+            output = output + "D";
+            input = input - 500;
         }
         while (input >= 400) {
-            s += "CD";
-            input -= 400;
+            output = output + "CD";
+            input = input - 400;
         }
         while (input >= 100) {
-            s += "C";
-            input -= 100;
+            output = output + "C";
+            input = input - 100;
         }
         while (input >= 90) {
-            s += "XC";
-            input -= 90;
+            output = output + "XC";
+            input = input - 90;
         }
         while (input >= 50) {
-            s += "L";
-            input -= 50;
+            output = output + "L";
+            input = input - 50;
         }
         while (input >= 40) {
-            s += "XL";
-            input -= 40;
+            output = output + "XL";
+            input = input - 40;
         }
         while (input >= 10) {
-            s += "X";
-            input -= 10;
+            output = output + "X";
+            input = input - 10;
         }
         while (input >= 9) {
-            s += "IX";
-            input -= 9;
+            output = output + "IX";
+            input = input - 9;
         }
         while (input >= 5) {
-            s += "V";
-            input -= 5;
+            output = output + "V";
+            input = input - 5;
         }
         while (input >= 4) {
-            s += "IV";
-            input -= 4;
+            output = output + "IV";
+            input = input - 4;
         }
         while (input >= 1) {
-            s += "I";
-            input -= 1;
+            output = output + "I";
+            input = input - 1;
         }
 
-        String output = "Input number "+inputNumber+" the corresponding number in Roman Numerals is: "+s;
-
-        return output;
+        String message = "Input number "+inputNumber+" the corresponding number in Roman Numerals is: "+output;
+        return message;
     }
 
 }
